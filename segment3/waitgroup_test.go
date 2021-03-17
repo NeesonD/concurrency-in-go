79ef69136c8f1985e58a964ac6118a2915af8bab
neeson <d1043358774@gmail.com>
2021-03-17 23:28:36 +0800

    Initial commit

---
+++ segment3/waitgroup_test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"sync"
+	"testing"
+)
+
+func BenchmarkContextSwitch(b *testing.B) {
+	var wg sync.WaitGroup
+	begin := make(chan struct{})
+	c := make(chan struct{})
+
+	var token struct{}
+	sender := func() {
+		defer wg.Done()
+		<-begin
+		for i := 0; i < b.N; i++ {
+			c <- token
+		}
+	}
+
+	receiver := func() {
+		defer wg.Done()
+		<-begin
+		for i := 0; i < b.N; i++ {
+			<-c
+		}
+	}
+
+	wg.Add(2)
+	go sender()
+	go receiver()
+	b.StartTimer()
+	// 通知开始某件事
+	close(begin)
+	// 等待某件事的结束
+	wg.Wait()
+}
